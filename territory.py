ce349d22ea0c537cd8c3c3dcb150b4943b7c902f
juanpmf13 <juanpablomonteirofernandes@id.uff.br>
2022-11-10 23:16:16 -0300

    territorios com cordenadas

--- territory.py
+++ territory.py
@@ -1,11 +1,12 @@
 from player import *
 
 class Territory:
-    def __init__(self, name, continent, neighbors_list, troops):
+    def __init__(self, name, continent, neighbors_list, troops, cordenada):
         self.name = name
         self.continent = continent
         self.neighbors = neighbors_list
         self.troops = troops
+        self.cordenadas = cordenada
         self.owner = None
 
     
