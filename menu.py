862e8fd5b345c838cf51dd02e32655ef46856e48
Xico <santos_bruno@id.uff.br>
2022-12-01 08:27:57 -0300

    Fazer botões facilmente e os 2 primeiros
    volto mais tarde

--- menu.py
+++ menu.py
@@ -32,7 +32,7 @@ class Iniciar_jogo(pygame.sprite.Sprite,fisica):
                 self.area = self.screen.get_rect()
                 self.valor=1
                 self.screen.blit(self.image,(self.screen.get_rect().centerx - self.image.get_rect()[2]/2,self.screen.get_rect().centery - self.image.get_rect()[3]/2))
-        
+
 class instancia_mapa(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -41,16 +41,34 @@ class instancia_mapa(pygame.sprite.Sprite):
         self.rect.center = self.screen.get_rect().center
         self.valor=1
         self.screen.blit(self.image,(0,0))
-        
+
 
     def update(self, a):
         pass
 
 
 class Menu:
+
+    def button(screen, position, text):
+        font = pygame.font.SysFont("Comic Sans MS", 50)
+        text_render = font.render(text, 1, (255, 0, 0))
+        x, y, w, h = text_render.get_rect()
+        x, y = position
+        pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w, y), 5)
+        pygame.draw.line(screen, (150, 150, 150), (x, y - 2), (x, y + h), 5)
+        pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w, y + h), 5)
+        pygame.draw.line(screen, (50, 50, 50), (x + w, y + h), [x + w, y], 5)
+        pygame.draw.rect(screen, (100, 100, 100), (x, y, w, h))
+        return screen.blit(text_render, (x, y))
+
+
+
     def __init__(self):
 
         pygame.init()
+        pygame.font.init()
+        Fonte = pygame.font.SysFont('Comic Sans MS', 30)
+
 
         #criação da tela
         size = width, height = 1600 ,  900
@@ -64,6 +82,7 @@ class Menu:
         background.fill((0, 0, 0 ))
 
         screen.blit(background, (0, 0))
+
         map= None
         novo_jogo= Iniciar_jogo()
         botoes=None
@@ -74,16 +93,28 @@ class Menu:
                         novo_jogo.update(True)
                         if(pygame.mouse.get_pressed(num_buttons=3)[0]== True):
                             map = instancia_mapa()
+                            pygame.display.flip()
                             novo_jogo = None
-                            game = GameLoop(3)
-                            game.start()
+                            quad1=Menu.button (screen,(1096,0), "1 Jogador")
+                            quad2=Menu.button (screen,(1296,0), "2 Jogadores")
+                            pygame.display.flip()
+
+
                     else:
                         novo_jogo.update(False)
                 else:
-                    pass
+                    if (event.type == pygame.MOUSEBUTTONDOWN):
+                        if quad1.collidepoint(pygame.mouse.get_pos()):
+                            game = GameLoop(1)
+                            game.start()
+                    if (event.type == pygame.MOUSEBUTTONDOWN):
+                        if quad2.collidepoint(pygame.mouse.get_pos()):
+                            game = GameLoop(2)
+                            game.start()
+
                 if event.type == QUIT:
                     return
             pygame.display.flip()
 Menu()
         
-        
+
